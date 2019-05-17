cb58e517691d71d123d97852a9ed5a5d05b79f46
Your Name <you@example.com>
2019-05-17 18:17:45 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,44 @@ require 'pry'
 
 class String
 
+  @@count = 0
   def sentence?
-    
+    if self.split('').pop == "."
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.split('').pop == "?"
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.split('').pop == "!"
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
+    array = self.split(/[.?!]/)
+    array.delete_if do |char|
+      char == ""
+    end
+    return array.length
 
+    # array.each do |char|
+    #   if char == "." || char == "?" || char == "!"
+    #     @@count+=1
+    #
+    #   end
+    # end
+    # return @@count
   end
 end
