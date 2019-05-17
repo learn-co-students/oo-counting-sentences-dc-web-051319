2ea4ac2673bf00ff21bf1c83ca26fae7902107a9
Your Name <you@example.com>
2019-05-17 18:22:34 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,8 +1,6 @@
 require 'pry'
 
 class String
-
-  @@count = 0
   def sentence?
     if self.split('').pop == "."
       return true
@@ -29,17 +27,7 @@ class String
 
   def count_sentences
     array = self.split(/[.?!]/)
-    array.delete_if do |char|
-      char == ""
-    end
-    return array.length
-
-    # array.each do |char|
-    #   if char == "." || char == "?" || char == "!"
-    #     @@count+=1
-    #
-    #   end
-    # end
-    # return @@count
+    array.delete_if { |char| char == ""}
+     array.length
   end
 end
--- spec/count_sentences_spec.rb
+++ spec/count_sentences_spec.rb
@@ -34,13 +34,13 @@ describe String do
 
   describe "#count_sentences" do
 
-    # it  "returns the number of sentences in a string" do
-    #   expect("one. two. three?".count_sentences).to eq(3)
-    # end
+    it  "returns the number of sentences in a string" do
+      expect("one. two. three?".count_sentences).to eq(3)
+    end
 
-    # it "returns zero if there are no sentences in a string" do
-    #   expect("".count_sentences).to eq(0)
-    # end
+    it "returns zero if there are no sentences in a string" do
+      expect("".count_sentences).to eq(0)
+    end
 
     it "returns the number of sentences in a complex string" do
       complex_string = "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
